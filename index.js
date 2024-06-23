0f4f387cadf5b090d29dc980e34b162dc309db3e
Maruf-Raju-pro <Muhammedmarufraju@gmail.com>
2024-06-23 12:32:26 +0600

    bids data posting and getting routes created

--- index.js
+++ index.js
@@ -4,21 +4,20 @@ const cors = require('cors');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
-const corsOptions= {
-     origin:['http://localhost:5173',],
-     credentials:true,
-     opstionSuccessStatus:200
-}
+const corsOptions = {
+  origin: ['http://localhost:5173'],
+  credentials: true,
+  opstionSuccessStatus: 200,
+};
 // midlewere--------
 app.use(cors(corsOptions));
 app.use(express.json());
-
 
 app.get('/', async (req, res) => {
   res.send('access to root outsie of the mongodb');
 });
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster1.6mzg5rv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,16 +31,54 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const jobCollections = client
-      .db('SolisOrbBase')
-      .collection('jobs');
-// getting all jobs data => 
-  app.get('/jobs',async(req,res)=>{
-    const result = await jobCollections.find().toArray();
-    res.send(result)
+    const jobCollections = client.db('SolisOrbBase').collection('jobs');
+    const bidCollections = client.db('SolisOrbBase').collection('bids');
+    // getting all jobs data =>
+    app.get('/jobs', async (req, res) => {
+      const result = await jobCollections.find().toArray();
+      res.send(result);
+    });
+    // getting a single item =>
+    app.get('/jobs/:id', async (req, res) => {
+      try {
+        const id = req.params.id;
+        if (!ObjectId.isValid(id)) {
+          res.status(400).send('Invalid id format');
+        }
+        const query = { _id: new ObjectId(id) };
+        const result = await jobCollections.findOne(query);
+        if (!result) {
+          res.status(404).send('data not found');
+        }
 
-
-  })
+        console.log(result);
+        res.send(result);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+    // getting all bids =>
+    app.get('/bids', async (req, res) => {
+      try {
+        const result = await bidCollections.find().toArray();
+        res.send(result);
+      } catch (err) {
+        console.log(err?.message);
+      }
+    });
+    app.post('/bids', async (req, res) => {
+      try {
+        const data = req.body;
+        if (!data) {
+          res.status(400).send('bad request data is not sended');
+        }
+        const result = await bidCollections.insertOne(data);
+        console.log('insert to bid collections success');
+        res.send(result);
+      } catch (err) {
+        console.log(err?.message);
+      }
+    });
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
     console.log(
